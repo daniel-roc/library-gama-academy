f1e5e8971e68f54f89099c1c93a6f6ab90ccddd4
Andrei Brancalião <108850397+Andrei-Brancaliao@users.noreply.github.com>
2022-08-10 16:06:58 -0300

    0001 criada (#6)

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
 
 public class Usuario {
+
 
     private String nome;
     private String cpf;
     private String telefone;
     private String matricula;
+
+    private ArrayList<Integer> listaCodigosExemplares = new ArrayList<Integer>(); //talvez usar um map para linkar usuario + codigo ?
 
     public Usuario(String nome, String cpf, String telefone, String matricula) {
         this.nome = nome;
@@ -44,5 +48,39 @@ public class Usuario {
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
+
+    public ArrayList<Integer> getListaCodigosExemplares() {
+        return listaCodigosExemplares;
+    }
+
+    public void setListaCodigosExemplares(ArrayList<Integer> listaCodigosExemplares) {
+        this.listaCodigosExemplares = listaCodigosExemplares;
+    }
+
+    public void associarUsuarioComExemplar(Exemplar exemplar){
+//        exemplarEmprestado++;
+        listaCodigosExemplares.add(exemplar.getCodigo());
+    }
+
+    public void desassociarUsuarioComExemplar(Exemplar exemplar){
+        if(listaCodigosExemplares.contains(exemplar.getCodigo())){
+            listaCodigosExemplares.remove(listaCodigosExemplares.indexOf(exemplar.getCodigo()));
+            System.out.println("Exemplar devolvido com sucesso");
+        }else{
+            System.out.println("Esse usuario não está com esse exemplar");
+        }
+    }
+
+
+//        listaCodigosExemplares.contains(listaExemplar.get(i).getCodigo());
+//        for (Exemplar exemplarPesquisa : listaExemplar) {
+//            for(int i = 0; i <listaExemplar.size(); i++) {
+//                for (int j = 0; j < listaExemplar.size(); j++) {
+//                    if (lista.getCodigo(i) == user.getListaCodigosExemplares().get(j)) {
+//                        return true;
+//                    }
+//                }
+//            }
+//        }
 
 }
